dd603b9661095c6770379bc895be8cbbe7abf745
Prince Singh <princesinghraajput.2000@gmail.com>
2025-01-10 12:16:49 +0530

    New file commited Diff.java

---
+++ Diff.java
@@ -0,0 +1,10 @@
+package gitcourse.letslearngit;
+
+public class Diff {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 10;
+
+        System.out.println(a-b);
+    }
+}
