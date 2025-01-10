46fb47137d1417d6826c403c62400f16258c2c47
Prince Singh <princesinghraajput.2000@gmail.com>
2025-01-10 12:45:05 +0530

    Commited all with multiply

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+package gitcourse.letslearngit;
+
+public class Multiply {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 10;
+
+        System.out.println(a*b);
+    }
+}
