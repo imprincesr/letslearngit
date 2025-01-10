280830764ffa766e726908cb74a732cd01605df7
Prince Singh <princesinghraajput.2000@gmail.com>
2025-01-10 12:19:48 +0530

    added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -3,6 +3,6 @@ class Sum{
 	public static void main(String args[]){
         int a = 10;
         int b = 20;
-        System.out.println(a+b);
+        System.out.println("The Sum is" + (a+b));
     }
 }
